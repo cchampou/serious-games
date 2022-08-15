42eb843e10fc7e6c18cc83c4893906f080bece08
cchampou <clement@champouillon.com>
2022-08-15 14:55:08 +0200

    feat: use mock data in game gallery

--- shop/src/components/gameCard/GameCard.tsx
+++ shop/src/components/gameCard/GameCard.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import Button from '../button/Button';
-import gamble from '../../assets/monop.jpg';
 import './styles.css';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { useNavigate } from 'react-router-dom';
 
 type props = {
-  name: string;
   index: number;
-};
+} & Pick<GameEntity, 'name' | 'image' | 'id'>;
 
-function GameCard({ name, index }: props) {
+function GameCard({ name, image, index, id }: props) {
+  const navigate = useNavigate();
+
+  const seeDetailsHandler = () => {
+    navigate(`/game/${id}`);
+  };
+
   return (
     <AnimationOnScroll animateIn="animate__zoomIn" delay={index * 100}>
       <section className="game-card">
-        <img src={gamble} alt="Gamble game" />
+        <img src={image} alt="Gamble game" />
         <h2>{name}</h2>
-        <Button>See more...</Button>
-        <Button>Buy now</Button>
+        <Button type="button" onClick={seeDetailsHandler}>
+          See more...
+        </Button>
+        {/*<Button>Buy now</Button>*/}
       </section>
     </AnimationOnScroll>
   );
--- shop/src/pages/Browse.tsx
+++ shop/src/pages/Browse.tsx
@@ -3,16 +3,15 @@ import Header from '../components/header/Header';
 import GameCard from '../components/gameCard/GameCard';
 import GameGallery from '../components/gameGallery/GameGallery';
 import aces from '../assets/aces.jpg';
-
-const games = ['Aces', 'Monopoly', 'Risk', 'Roulette', 'Texas Holdem'];
+import games from '../data/games';
 
 function Browse() {
   return (
     <>
       <Header />
       <GameGallery>
-        {games.map((name, index) => (
-          <GameCard name={name} index={index} />
+        {games.map((game, index) => (
+          <GameCard index={index} {...game} />
         ))}
       </GameGallery>
       <img src={aces} alt="aces" id="aces" />
