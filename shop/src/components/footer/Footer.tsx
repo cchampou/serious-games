50634f5e68aabfa64345d61aba81ea4c9eebdb10
cchampou <clement@champouillon.com>
2022-08-15 15:51:15 +0200

    feat: footer with all necessary links

--- shop/src/components/footer/Footer.tsx
+++ shop/src/components/footer/Footer.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './styles.css';
+import { NavLink } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2020 - Michael</p>
+      <NavLink to="/about">Homepage</NavLink>
+      <NavLink to="/about">Browse Games</NavLink>
+      <NavLink to="/about">About me</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <p>
+        <i>Future copyright notice here</i>
+      </p>
+      <NavLink to="/about">Legal</NavLink>
+      <NavLink to="/contact">Data privacy</NavLink>
+      <a
+        href="https://github.com/cchampou/serious-games"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Source code on Github
+      </a>
     </footer>
   );
 }
