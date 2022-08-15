07b91bac823dd74ee7772581bbace8aea1f0b975
cchampou <clement@champouillon.com>
2022-08-15 18:09:06 +0200

    feat: lead text customizable

--- shop/src/components/lead/Lead.tsx
+++ shop/src/components/lead/Lead.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './styles.css';
 
-function Lead() {
-  return (
-    <p className="lead">
-      Want to get your organization ready for the future? Want to strengthen
-      your team and get them to make better choices? Also, that employees can
-      experiment in a safe environment, learn from each other or experience the
-      impact of their own choices? Serious games can achieve unprecedented
-      results in all these scenarios.
-    </p>
-  );
+type Props = {
+  text: string;
+};
+
+function Lead({ text }: Props) {
+  return <p className="lead">{text}</p>;
 }
 
 export default Lead;
--- shop/src/pages/Homepage.tsx
+++ shop/src/pages/Homepage.tsx
@@ -10,12 +10,17 @@ type Props = Record<string, never>;
 const TITLE = 'Play the game';
 const TEXT_CONTENT =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const LEAD_TEXT = `Want to get your organization ready for the future? Want to strengthen
+  your team and get them to make better choices? Also, that employees can
+  experiment in a safe environment, learn from each other or experience the
+  impact of their own choices? Serious games can achieve unprecedented
+  results in all these scenarios.`;
 
 function Homepage() {
   return (
     <>
       <Jumbotron callToAction="/browse" />
-      <Lead />
+      <Lead text={LEAD_TEXT} />
       <Aside title={TITLE} textContent={TEXT_CONTENT} image={poker} />
       <Aside title={TITLE} textContent={TEXT_CONTENT} image={poker} reverse />
     </>
